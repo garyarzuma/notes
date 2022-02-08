882a6d61efa15e192a287c510297d73023f7ca3e
garyarzumanyan <garyarzumanyan@gmail.com>
2022-02-07 23:33:59 -0800

    Added cypress tests for frontend

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -1,6 +1,12 @@
-/* eslint-disable no-undef */
 describe('Note app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -11,10 +17,48 @@ describe('Note app', function() {
 
   it('login form can be opened', function() {
     cy.contains('log in').click()
-    cy.get('#username').type('garyroolz')
-    cy.get('#password').type('starpower')
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('salainen')
     cy.get('#login-button').click()
 
-    cy.contains('garybob logged-in')
+    cy.contains('Matti Luukkainen logged-in')
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: 'mluukkai', password: 'salainen' })
+    })
+
+    describe('and multiple notes exists', function () {
+      beforeEach(function () {
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
+        cy.contains('show all').click()
+      })
+
+      it('one of those can be made important', function () {
+        cy.contains('second note')
+          .contains('make important')
+          .click()
+
+        cy.contains('second note')
+          .contains('make not important')
+      })
+    })
+  })
+
+  it('login fails with wrong password', function() {
+    cy.contains('log in').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+
+    cy.get('.error').should('contain', 'Wrong credentials')
+    cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    cy.get('.error').should('have.css', 'border-style', 'solid')
+
+    cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
   })
 })
+
